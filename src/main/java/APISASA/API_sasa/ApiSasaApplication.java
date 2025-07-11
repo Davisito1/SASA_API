555dd7aa4477aa1e5439cbb9533f8bfd33f4b0fe
Davis <20220354@ricaldone.edu.sv>
2025-07-11 10:36:17 -0600

    api

---
+++ src/main/java/APISASA/API_sasa/ApiSasaApplication.java
@@ -0,0 +1,13 @@
+package APISASA.API_sasa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSasaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSasaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/APISASA/API_sasa/ApiSasaApplicationTests.java
@@ -0,0 +1,13 @@
+package APISASA.API_sasa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSasaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
